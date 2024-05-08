cea4ec941740a74e7977daa398794e20a5dccf44
Michael lemito <delovoy.lemit@gmail.com>
2024-05-09 00:09:19 +0300

    Начало
    Создал работу с сокетом и работу с сетью (ее инициализация)

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <stdio.h>
+#include <sys/socket.h>
+
+#define PORT 8080
+
+int main()
+{
+    // создаем сокет ~= "шина" клиент-сервер
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1)
+    {
+        perror("Ошибка");
+        return 1;
+    }
+    puts("Сокет успешно создан\n");
+
+    // создаем адрес хоста
+    struct sockaddr_in host_addr;
+    int host_addr_len = sizeof(host_addr);
+
+    // инициализируем адрес
+    // используем ipv4
+    host_addr.sin_family = AF_INET;
+    // сидим на определенном порту; содержится в виде 4 байт сети
+    host_addr.sin_port = htons(PORT);
+    // сидим на 0.0.0.0
+    host_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(sockfd, &host_addr, host_addr_len)){
+        perror("Не удалось связать адрес и сокет");
+        return 1;
+    }
+    puts("Связано успешно!");
+
+    
+
+    return 0;
+}
