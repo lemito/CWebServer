f810a882503e0f286304588ba97d502d8b7c9352
Michael lemito <delovoy.lemit@gmail.com>
2024-05-09 17:15:15 +0300

    Рабочая форма

--- main.c
+++ main.c
@@ -24,9 +24,9 @@ char *response_creator(const int status, const char *content_type, const char *m
         return NULL;
     }
     snprintf(response, 4096, "HTTP/1.0 %d OK\r\n"
-                                         "Server: CWebServer\r\n"
-                                         "Content-type: %s\r\n\r\n"
-                                         "<html>%s</html>\r\n",
+                             "Server: CWebServer\r\n"
+                             "Content-type: %s\r\n\r\n"
+                             "%s",
              status, content_type, message);
     return response;
 }
@@ -34,7 +34,7 @@ char *response_creator(const int status, const char *content_type, const char *m
 char *text_creator(const char *message)
 {
     size_t message_len = strlen(message);
-    size_t response_len = message_len + 6;
+    size_t response_len = message_len + 8;
     char *response = malloc(response_len);
     if (response == NULL)
     {
@@ -46,33 +46,32 @@ char *text_creator(const char *message)
     return response;
 }
 
-
-
-int main()
+char *form_creator(const char *action, char *const value, char *const id)
 {
-    char buffer[BUFFER_SIZE];
-    char *test_resp = response_creator(200, "text/html", text_creator("Hello, world!"));
-    printf("%s", test_resp);
+    char *response = malloc(1024);
 
-    // создаем сокет ~= "шина" клиент-сервер
+    snprintf(response, 1024, "<form action='%s' method='post'><input value='%s' id='%s' type='text' name='name' /><button type='submit'>Submit</button></form>", action, value, id);
+    return response;
+}
+
+int setup_socket()
+{
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1)
     {
         perror("Ошибка");
         exit(EXIT_FAILURE);
     }
-    puts("Сокет успешно создан\n");
+    puts("Сокет успешно создан");
+    return sockfd;
+}
 
-    // создаем адрес хоста
+void setup_server(int sockfd)
+{
     struct sockaddr_in host_addr;
     socklen_t host_addr_len = sizeof(host_addr);
-
-    // инициализируем адрес
-    // используем ipv4
     host_addr.sin_family = AF_INET;
-    // сидим на определенном порту; содержится в виде 4 байт сети
     host_addr.sin_port = htons(PORT);
-    // сидим на 0.0.0.0
     host_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     if (bind(sockfd, (struct sockaddr *)&host_addr, host_addr_len))
@@ -82,49 +81,99 @@ int main()
     }
     puts("Связано успешно!");
 
-    // прослушиваем входящий подключения; не мелочимся и берем максимум
     if (listen(sockfd, SOMAXCONN) != 0)
     {
         perror("Прослушивание неудачно");
         exit(EXIT_FAILURE);
     }
-
     puts("Сервер готов к пролушиванию соединений");
-
     printf("Сервер готов по адресу 0.0.0.0:8080\n");
+}
 
-    while (1)
+void handle_client(int new_sockfd, char *test_resp)
+{
+    char buffer[BUFFER_SIZE];
+    ssize_t valRead = read(new_sockfd, buffer, BUFFER_SIZE);
+    if (valRead < 0)
     {
-        // принимаем входящие подключения
-        int new_sockfd = accept(sockfd, (struct sockaddr *)&host_addr, (socklen_t *)&host_addr_len);
+        perror("Чтение не успешно");
+        close(new_sockfd);
+        return;
+    }
 
-        if (new_sockfd < 0)
+    if (strncmp(buffer, "POST", 4) == 0)
+    {
+        char *body = strstr(buffer, "\r\n\r\n");
+        if (body)
         {
-            perror("Ошибка при подключении");
+            body += 4;
+            char *name_start = strstr(body, "name=");
+            if (name_start)
+            {
+                name_start += 5;
+                char *name_end = strchr(name_start, '&');
+                if (!name_end)
+                    name_end = name_start + strlen(name_start);
+
+                char name[100];
+                strncpy(name, name_start, name_end - name_start);
+                name[name_end - name_start] = '\0';
+
+                char *greeting_msg = strdup("Hello ");
+                if (!greeting_msg)
+                {
+                    fprintf(stderr, "Не удалось выделить память\n");
+                    return;
+                }
+
+                strncat(greeting_msg, name, sizeof(name) - strlen(greeting_msg) - 1);
+                char *greeting = text_creator(greeting_msg);
+                char *response = response_creator(200, "text/html", greeting);
+                printf("response form = %s\n", response);
+
+                ssize_t valWrite = write(new_sockfd, response, strlen(response));
+                if (valWrite < 0)
+                {
+                    perror("Запись не успешна");
+                }
+                FREE_AND_NULL(greeting_msg);
+                FREE_AND_NULL(response);
+                FREE_AND_NULL(greeting);
+            }
         }
-
-        puts("Кто-то подключился | Успешно!");
-
-        // читаем с потока в буфер
-        ssize_t valRead = read(new_sockfd, buffer, BUFFER_SIZE);
-        if (valRead < 0)
-        {
-            perror("Чтение не успешно");
-            close(new_sockfd);
-            continue;
-        }
-
-        // пишем в поток
+    }
+    else
+    {
         ssize_t valWrite = write(new_sockfd, test_resp, strlen(test_resp));
         if (valWrite < 0)
         {
             perror("Запись не успешна");
-            close(new_sockfd);
+        }
+    }
+
+    close(new_sockfd);
+}
+
+int main()
+{
+    int sockfd = setup_socket();
+    setup_server(sockfd);
+
+    char *test_resp = response_creator(200, "text/html", form_creator("/", "", "name"));
+    printf("%s", test_resp);
+
+    while (1)
+    {
+        struct sockaddr_in host_addr;
+        socklen_t host_addr_len = sizeof(host_addr);
+        int new_sockfd = accept(sockfd, (struct sockaddr *)&host_addr, &host_addr_len);
+        if (new_sockfd < 0)
+        {
+            perror("Ошибка при подключении");
             continue;
         }
-
-        // закрываем подключение
-        close(new_sockfd);
+        puts("Кто-то подключился | Успешно!");
+        handle_client(new_sockfd, test_resp);
     }
 
     FREE_AND_NULL(test_resp);
